d35dc139ff2def81b066ac239cc4727a17d756b1
Jhony Vidal <jhony.oliveira@mulesoft.com>
2020-11-08 13:28:07 -0300

    Add Json Metadata and Unmarshal to User object

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,14 +1,33 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/aipetto/go-aipetto-users-api/domain/users"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
 
-func GetUser(c *gin.Context) {
+func CreateUser(c *gin.Context) {
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		// TODO: Handler error
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err.Error())
+		// TODO Handle json error
+		return
+	}
+	fmt.Println(user)
+	fmt.Println(string(bytes))
+	fmt.Println(err)
 	c.String(http.StatusNotImplemented, "to implement")
 }
 
-func CreateUser(c *gin.Context) {
+func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "to implement")
 }
